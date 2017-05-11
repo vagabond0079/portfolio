911805151c0765eed460575e88b026a53ebbda58
derekcfarmer <derek.farmer@gmail.com>
2017-05-10 22:10:30 -0700

    created new js files, beginning template

---
+++ project.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function Project (name, author, date, gitUrl, imageUrl, description){
+  this.name = name;
+  this.author = author;
+  this.date = date;
+  this.gitUrl = gitUrl;
+  this.imageUrl = imageUrl;
+  this.description = description;
+}
