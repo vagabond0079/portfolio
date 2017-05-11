702ab198e3965fc9959bcd401e21ce6dee738466
derekcfarmer <derek.farmer@gmail.com>
2017-05-11 00:01:56 -0700

    project data populating to the view, issues with datetime

--- project.js
+++ project.js
@@ -1,10 +1,32 @@
 'use strict';
 
-function Project (name, author, date, gitUrl, imageUrl, description){
-  this.name = name;
-  this.author = author;
-  this.date = date;
-  this.gitUrl = gitUrl;
-  this.imageUrl = imageUrl;
-  this.description = description;
+var projects = [];
+
+function Project (projectObject){
+  this.name = projectObject.name;
+  this.author = projectObject.author;
+  this.date = projectObject.date;
+  this.gitUrl = projectObject.gitUrl;
+  this.imageUrl = projectObject.imageUrl;
+  this.description = projectObject.description;
 }
+
+Project.prototype.toHtml = function(){
+  var $newProject = $('article.template').clone();
+  $newProject.removeClass('template');
+
+  $newProject.find('h1').html(this.name);
+  $newProject.find('.byline span').html(this.author);
+  $newProject.find('time[pubdate]').attr('datetime', this.date);
+  $newProject.find('.description').html(this.description);
+
+  return $newProject;
+}
+
+projectData.forEach(function(projectObject){
+  projects.push(new Project(projectObject));
+});
+
+projects.forEach(function(project){
+  $('.projects').append(project.toHtml());
+});
