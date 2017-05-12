5fb36075708a41e6966956629c51b234e6d7150d
derekcfarmer <derek.farmer@gmail.com>
2017-05-12 07:30:21 -0700

    three projects, tab on click, styling changes

--- view.js
+++ view.js
@@ -1,6 +1,26 @@
 'use strict';
 
+var projectView = [];
 
-$('#hamburger').on('click', function(){
-  $('.navButton').toggleClass('is-expanded');
+projectView.hamburgerMenu = function(){
+  $('#hamburger').on('click', function(){
+    $('.navButton').toggleClass('is-expanded');
+  });
+};
+
+projectView.handleMainNav = function() {
+  $('.main-nav').on('click', '.navButton', function() {
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
+    $('#hamburger').click();
+  });
+
+  $('.main-nav .navButton:first').click();
+  $('#hamburger').click();
+};
+
+
+$(document).ready(function() {
+  projectView.hamburgerMenu();
+  projectView.handleMainNav();
 });
