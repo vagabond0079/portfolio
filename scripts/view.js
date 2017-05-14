a5c12fda9da57ab79820ea27aa2638d25f49898f
derekcfarmer <derek.farmer@gmail.com>
2017-05-13 19:24:08 -0700

    clean home screen with transitioned hi im derek. working on slide up on about me

--- scripts/view.js
+++ scripts/view.js
@@ -2,25 +2,49 @@
 
 var projectView = {};
 
+var currentView = '';
+
+projectView.home = function(){
+  $('.tab-content').hide();
+  $('#hamburger').hide();
+  $('#home').append('<div id="hi" class="my-name">Hi</div></br>');
+  $('#home').append('<div id="me" class="my-name">I\'m Derek Farmer</div>');
+  $('#hi, #me').hide();
+  $('#home').show();
+  $('#hi').fadeIn(2000);
+  $('#me').delay(2200).fadeIn(2500);
+  $('#hamburger').delay(4000).fadeIn();
+  currentView = 'home';
+};
+
 projectView.hamburgerMenu = function(){
   $('#hamburger').on('click', function(){
     $('.navButton').toggleClass('is-expanded');
   });
 };
 
-projectView.handleMainNav = function() {
-  $('.main-nav').on('click', '.navButton', function() {
-    $('.tab-content').hide();
-    $('#' + $(this).data('content')).fadeIn();
-    // $('#hamburger').click();
+projectView.handleAboutClick = function() {
+  $('.main-nav').on('click', '#aboutme', function(){
+    console.log('clickie');
+    if(currentView==='home'){
+      console.log('homie');
+      $('#home').animate('margin-top', '25vh');
+    }
   });
-
-  $('.main-nav .navButton:first').click();
-  // $('#hamburger').click();
 };
+
+//
+// projectView.handleMainNav = function() {
+//   $('.main-nav').on('click', '.navButton', function() {
+//     $('.tab-content').hide();
+//     $('#' + $(this).data('content')).fadeIn();
+//   });
+// };
 
 
 $(document).ready(function() {
+  projectView.home();
   projectView.hamburgerMenu();
-  projectView.handleMainNav();
+  projectView.handleAboutClick();
+  // projectView.handleMainNav();
 });
