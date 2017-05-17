bdf7649308615d0744f9bcc21c1ce6b3bf262b57
derekcfarmer <derek.farmer@gmail.com>
2017-05-17 15:34:47 -0700

    server working

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
-
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
-// Remember to install express, and be sure that it's been added to your package.json as a dependency
-// There is also a package here called body-parser, which is required in for use in a new route.
-// Be sure to install that and save it as a dependency after you create your package.json.
 const express = require('express');
 const app = express();
-
 
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
