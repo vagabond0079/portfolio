9b9744efb82945cb2829b81b004819849f02a806
derekcfarmer <derek.farmer@gmail.com>
2017-05-26 10:01:03 -0700

    added server constants for dependencies

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 'use strict';
 
+const pg = require('pg');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const requestProxy = require('express-request-proxy');
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
+const conString = process.env.DATABASE_URL || 'postgres://localhost:5432';
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', err => console.error(err));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
 
 function proxyGitHub(request, response) {
