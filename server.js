7de0c5a0e854f8728d8c7cdd41bc68681613be69
derekcfarmer <derek.farmer@gmail.com>
2017-05-26 09:53:06 -0700

    added pg and express-request-proxy dependencies

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-
+const requestProxy = require('express-request-proxy');
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
